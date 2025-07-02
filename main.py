6f437968cae77565b1656728bcfc8082015537b5
Chenyang Tang <gteeice@163.com>
2025-07-02 16:36:11 +0800

    update

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import streamlit as st
 from pathlib import Path
 HOME_PATH = Path.home()
 
-
+MARKDOWN_TEMPLATE = """{format}
+{content}
+"""
 if __name__ == '__main__':
     # Left Side Bar
     with st.sidebar:
@@ -24,13 +26,13 @@ if __name__ == '__main__':
         os.makedirs(os.path.dirname(file_full_path), exist_ok=True)
         with open(file_full_path, 'w') as f:
             f.write(input_text)
-            st.success(f"File '{file_name}' has been created successfully.")
+            st.success(f"File '{file_name}' has been updated successfully.")
 
     if os.path.exists(file_full_path):
         with open(file_full_path, 'r') as f:
             st.markdown("### File Content")
             file_content = f.read()
-            st.write(file_content)
+            st.markdown(MARKDOWN_TEMPLATE.format(format="text", content=file_content))
 
 
 
