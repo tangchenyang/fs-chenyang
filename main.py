bfb2df370c82386df7081df3fdb0317d7f8bef4a
Chenyang Tang <gteeice@163.com>
2025-07-03 11:43:41 +0800

    update

--- main.py
+++ main.py
@@ -9,24 +9,26 @@ MARKDOWN_TEMPLATE = """```{format}
 {content}
 """
 if __name__ == '__main__':
-    # Left Side Bar
-    with st.sidebar:
-        st.markdown(f"# Configurations")
-        username = st.text_input("Username", placeholder="Enter your username")
-        user_home = f"{HOME_PATH}/fs-users/{username}"
+    # User
+    st.markdown(f"# Configurations")
+    username = st.text_input("Username", placeholder="Enter your username")
+    user_home = f"{HOME_PATH}/fs-users/{username}"
 
     #
     input_text = st.text_area("Enter your text", placeholder="Enter your text")
-    file_name = "abc.txt" # todo
+    file_name = "temp.txt"
     file_full_path = f"{user_home}/{file_name}"
 
-    btn = st.button("Save File")
+    col1, col2 = st.columns(2)
+    btn = col1.button("Save Content", use_container_width=True)
 
     if btn:
         os.makedirs(os.path.dirname(file_full_path), exist_ok=True)
         with open(file_full_path, 'w') as f:
             f.write(input_text)
-            st.success(f"File '{file_full_path}' has been updated successfully.")
+            st.success(f"Saved content to '{file_full_path}' successfully.")
+
+    refresh_btn = col2.button("Refresh Content", use_container_width=True)
 
     if os.path.exists(file_full_path):
         with open(file_full_path, 'r') as f:
